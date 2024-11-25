cd63f1897f8871a5e57dfa98649c48cd5af4c487
galofran <75098469+galofran@users.noreply.github.com>
2024-11-25 19:18:54 +0100

    Anotacion @Component añadida a JwtAuthorizationFilter

--- src/main/java/com/example/security/filters/JwtAuthorizationFilter.java
+++ src/main/java/com/example/security/filters/JwtAuthorizationFilter.java
@@ -7,6 +7,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.security.jwt.JwtUtils;
@@ -17,6 +18,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter{
 
 	@Autowired
